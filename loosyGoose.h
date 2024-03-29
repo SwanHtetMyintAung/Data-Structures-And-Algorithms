776ca5c91c6cba581c174116bfdb7c98b7e16837
Tom Rudy <89239013+SwanHtetMyintAung@users.noreply.github.com>
2024-03-29 12:16:39 +0630

    updates loosyGoose.h and test file

--- loosyGoose.h
+++ loosyGoose.h
@@ -12,6 +12,15 @@ struct Link_List{
     int data;
     struct Link_List* next;
 };
+struct Stack_Linklist{
+    struct Link_List* top;
+};
+struct Tree_Node{
+    int data;
+    struct Tree_Node *left;
+    struct Tree_Node *right;
+
+};
 //stack
 void stack_initialize(struct Stack* stack);
 void stack_push(struct Stack* stack , int data);
@@ -27,9 +36,18 @@ void ll_append(struct Link_List** head , int data);
 void ll_insert_after(struct Link_List** head,int target,int data);
 void ll_delete_node(struct Link_List** head , int target);
 void ll_show_all_node(struct Link_List* head);
+//stack with link list
+int stack_ll_is_empty(struct Stack_Linklist *stack);
+void stack_ll_initialize(struct Stack_Linklist* stack);
+void stack_ll_push(struct Stack_Linklist *stack , int data);
+//binary search tree
+void inorderTraversal(struct Tree_Node* root);
+struct Tree_Node* create_tree_node(int data);
+struct Tree_Node* insert_tree_node(struct Tree_Node* root , int data);
+struct Tree_Node* find_min_tree_node(struct Tree_Node* root);
+struct Tree_Node* delete_tree_node(struct Tree_Node* root,int data);
 
-
-//stack
+//*************************stack
 void stack_initialize(struct Stack* stack){
     stack->top = -1;
 }
@@ -80,7 +98,7 @@ void stack_log(struct Stack* stack){
     printf("Done!\n");
 }
 
-//link list 
+//***********************link list 
 struct Link_List* ll_create_node(int number){
     struct Link_List* node = (struct Link_List*)malloc(sizeof(struct Link_List));
     node->data  = number;
@@ -171,5 +189,153 @@ void ll_show_all_node(struct Link_List* head){
 
 }
 
+//************************stack with linklist
+int stack_ll_is_empty(struct Stack_Linklist *stack){
+    return (stack->top == NULL);
+}
+void stack_ll_initialize(struct Stack_Linklist* stack){
+    stack->top = NULL;
+}
+void stack_ll_push(struct Stack_Linklist *stack , int data){
+    struct Link_List* newNode = ll_create_node(data);
+    newNode->next = stack->top;
+    stack->top = newNode;
+}
+int stack_ll_pop(struct Stack_Linklist *stack ){
+    if(stack_ll_is_empty(stack)){
+        printf("stack is empty!\n");
+        exit(1);
+    }
+    struct Link_List *temp = stack->top;
+    int data = temp->data;
+    stack->top = stack->top->next;
+    free(temp);
+    return data;
+}
+int stack_ll_peek(struct Stack_Linklist *stack){
+    if(stack_ll_is_empty(stack)){
+        printf("stack is empty!\n");
+        exit(1);
+    }
+    return stack->top->data;
+}
+
+/*******************Binary Search Tree**********/
+
+struct Tree_Node* create_tree_node(int data){
+    struct Tree_Node* newNode = (struct Tree_Node*)malloc(sizeof(struct Tree_Node));
+    if(!newNode){
+        printf("Memory Allocation Failed!");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+struct Tree_Node* insert_tree_node(struct Tree_Node* root , int data){
+    //create a new node if current one is NULL
+    if(root  == NULL){
+        root = create_tree_node(data);
+    }else if(data <= root->data){
+        //if the data is LESS (or equal) than the current node,
+        //we will call the function again but the left node of current ROOT as root
+        root->left = insert_tree_node(root->left,data);
+    }else{
+        //same but if the data is GREATER than current node
+        root->right = insert_tree_node(root->right,data);
+    }
+    //return the node
+    //this line will return the "created node" if the the first condion is met
+    //until the we haven't add a new Node to the tree, 
+    //this function will recursive
+    return root;
+}
+void inorderTraversal(struct Tree_Node* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+//find and return a node with smallest value
+struct Tree_Node* find_min_tree_node(struct Tree_Node* root){
+    if(root == NULL){
+        return root;
+    }
+    if(root->left == NULL){
+        return root;
+    }else{
+        root->left = find_min_tree_node(root->left);
+    }
+}
+struct Tree_Node* delete_tree_node(struct Tree_Node* root,int data){
+    if(root == NULL){
+        return root;
+    }
+    //will go to left node
+    if(data < root->data){
+        root->left = delete_tree_node(root->left , data);
+        return root;
+    //if the data was greater than current node , it will continue to the right
+    }else if(data > root->data){
+        root->right = delete_tree_node(root->right , data);
+        return root;
+    //case for if we found the data .
+    //data == root->data
+    }else{
+        //case 1: root has NO children
+        if((root->left==NULL)&&(root->right==NULL)){
+            //to store the Current ROOT and delete it later
+            struct Tree_Node* temp = root;
+            free(temp);
+            return root;
+        //case 2 : root has 1 child
+        }else if(root->left == NULL){
+            //to store the Current ROOT and delete it later
+            struct Tree_Node* temp = root;
+            root = root->right;
+            free(temp);
+            return root;
+        }else if(root->right == NULL){
+            //to store the Current ROOT and delete it later
+            struct Tree_Node* temp = root;
+            root = root->left;
+            free(temp);
+            return root;
+        //case 3: root has 2 children
+        }else if((root->left)&&(root->right)){
+            //find the "smallest" value in the RIGHT side
+                struct Tree_Node *parent =  root;
+                //finding the smallest value in the right sub-tree of the root
+                struct Tree_Node *child  = root->right;
+                while(child->left != NULL){
+                    //keep track of the parent of the child
+                    parent = child;
+                    //go to next "left" node if it exists
+                    child = child->left;
+                }
+                //the "child" node we have to delete is the child of root
+                //this mean , we didn't need to find the child node which lead the "parent" node
+                //to be the same as starting point
+                if(parent == root){
+                    parent->right = child->right;
+                //if we somehow managed to find the smallest node ,
+                //which mean this "child" node has no "left" node 
+                //but there might be "right" node ,
+                //we are basically linking the 
+                //"parent" of the node we have to delete and the
+                //the only "child"(in this case we know it could only be the right side)
+                }else{
+                    parent->left = child->right;
+                }
+                //replace the value
+                root->data = child->data;
+                free(child);
+                return root;
+        }
+
+    }
+}
 
 #endif
--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 #include"loosyGoose.h"
 
 int main(){
-    //for testing stack
+    /****************8for testing stack*****************/
     // struct Stack stack;
     
     // stack_initialize(&stack);
@@ -24,18 +24,56 @@ int main(){
     // stack_log(&stack);
 
 
-    //testing link list
-    struct Link_List* head = NULL;
-    ll_push(&head,10);
-    ll_append(&head,20);
-    ll_push(&head,30);
-    ll_show_all_node(head);
-    ll_insert_after(&head,10,40);
-    ll_show_all_node(head);
-    ll_delete_node(&head,20);
-    ll_show_all_node(head);
+    /*********************testing link list******************/
+    // struct Link_List* head = NULL;
+    // ll_push(&head,10);
+    // ll_append(&head,20);
+    // ll_push(&head,30);
+    // ll_show_all_node(head);
+    // ll_insert_after(&head,10,40);
+    // ll_show_all_node(head);
+    // ll_delete_node(&head,20);
+    // ll_show_all_node(head);
+
+    /**********************testing stack with link list **********************/ 
+    // struct Stack_Linklist* stack = (struct Stack_Linklist*)malloc(sizeof(struct Stack_Linklist));
+    // stack_ll_initialize(stack);
+    // stack_ll_push(stack,10);
+    // printf("peek:%d\n",stack_ll_peek(stack));
+    // stack_ll_push(stack,20);
+    // printf("peek:%d\n",stack_ll_peek(stack));
+    // stack_ll_pop(stack);
+    // printf("peek:%d\n",stack_ll_peek(stack));
+    // stack_ll_push(stack,30);
+    // printf("peek:%d\n",stack_ll_peek(stack));
+    /*note:
+        since this stack use the linklist to store data.
+        you can re-use the function from the link list section .
+        just treate the "top" as head 
+    */
+    //ll_show_all_node(stack->top);
+    //ll_append(&(stack->top),50);
+    //ll_show_all_node(stack->top);
+
+    /**************Testing BST*******************/
+    struct Tree_Node* root;
+
+    root = insert_tree_node(root,10);
+    root = insert_tree_node(root,20);
+    root = insert_tree_node(root,30);
+    root = insert_tree_node(root,25);
+    root = insert_tree_node(root,9);
+
+    printf("Inorder traversal before deleting:");
+    inorderTraversal(root);
+    printf("\n");
+
+    root = delete_tree_node(root,10);
+
+    printf("Inorder traversal After deleting 10:");
+    inorderTraversal(root);
+    printf("\n");
 
     
-
     return 0;
 }
